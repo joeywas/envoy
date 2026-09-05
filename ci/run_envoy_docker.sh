c7b734086aa31c19f02f931d8122f1058f83223b
Joseph <joey.was@gmail.com>
2026-09-04 23:39:27 -0700

    Add hyperv isolation to Envoy Docker run command
    Signed-off-by: Joseph <joey.was@gmail.com>

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -61,6 +61,6 @@ export ENVOY_BUILD_IMAGE
 exec docker compose \
     -f "${SCRIPT_DIR}/docker-compose.yml" \
     ${ENVOY_DOCKER_PLATFORM:+-p "$ENVOY_DOCKER_PLATFORM"} \
-    run \
+    run --isolation=hyperv \
     --rm \
     "${COMPOSE_SERVICE}"
